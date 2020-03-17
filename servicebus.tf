72870db161c9d91e08bed7551374f7518b24fbe7
rahman124 <mohammed.rahman@hmcts.net>
2020-03-17 12:29:13 +0000

    merged in master to remove .git suffix

--- servicebus.tf
+++ servicebus.tf
@@ -1,12 +1,13 @@
 locals {
   evidenceshare_topic_name        = "${var.product}-evidenceshare-topic-${var.env}"
   evidenceshare_subscription_name = "${var.product}-evidenceshare-subscription-${var.env}"
+  notifications_subscription_name = "${var.product}-notifications-subscription-${var.env}"
   servicebus_namespace_name       = "${var.product}-servicebus-${var.env}"
   resource_group_name             = "${azurerm_resource_group.rg.name}"
 }
 
 module "servicebus-namespace" {
-  source              = "git@github.com:hmcts/terraform-module-servicebus-namespace.git"
+  source              = "git@github.com:hmcts/terraform-module-servicebus-namespace?ref=master"
   name                = "${local.servicebus_namespace_name}"
   location            = "${var.location}"
   resource_group_name = "${azurerm_resource_group.rg.name}"
@@ -15,15 +16,23 @@ module "servicebus-namespace" {
 }
 
 module "evidenceshare-topic" {
-  source                = "git@github.com:hmcts/terraform-module-servicebus-topic.git"
+  source                = "git@github.com:hmcts/terraform-module-servicebus-topic?ref=master"
   name                  = "${local.evidenceshare_topic_name}"
   namespace_name        = "${local.servicebus_namespace_name}"
   resource_group_name   = "${local.resource_group_name}"
 }
 
 module "evidenceshare-subscription" {
-  source                = "git@github.com:hmcts/terraform-module-servicebus-subscription.git"
+  source                = "git@github.com:hmcts/terraform-module-servicebus-subscription?ref=master"
   name                  = "${local.evidenceshare_subscription_name}"
+  namespace_name        = "${local.servicebus_namespace_name}"
+  resource_group_name   = "${local.resource_group_name}"
+  topic_name            = "${local.evidenceshare_topic_name}"
+}
+
+module "notifications-subscription" {
+  source                = "git@github.com:hmcts/terraform-module-servicebus-subscription?ref=master"
+  name                  = "${local.notifications_subscription_name}"
   namespace_name        = "${local.servicebus_namespace_name}"
   resource_group_name   = "${local.resource_group_name}"
   topic_name            = "${local.evidenceshare_topic_name}"
